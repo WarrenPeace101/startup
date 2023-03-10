f5850d8e22c160af24171631a7786a9b355d46a3
WarrenPeace101 <mangum.warren99@gmail.com>
2023-03-09 23:20:31 -0700

    I FINALLY GOT THE CARS TO MOVE WOOWHOOOOO!

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ document.addEventListener("keydown", (e) => {
     if (e.key === "a") {
 
         greenFrogEl.style.left = parseFloat(greenFrogEl.style.left || 0) - 5 + 'px';
+        
     }
     else if (e.key === "w") {
 
@@ -30,19 +31,71 @@ document.addEventListener("keydown", (e) => {
 
 });
 
-const truckEl = document.getElementByID("#truckID");
-
-function driveRight() {
-
-}
+const truckOneEl = document.getElementById("truckOneID");
+const truckTwoEl = document.getElementById("truckTwoID");
+const truckThreeEl = document.getElementById("truckThreeID");
+const logOneEl = document.getElementById("logOne");
+const logThreeEl = document.getElementById("logThree");
 
 async function driveLeft() {
 
-    for (let i = 0; i < 10; i++) {
+    while (1) {
 
-        truckEl.style.left = parseFloat(truckEl.style.left || 0) - 5 + "px";
-        
+        truckOneEl.style.left = parseFloat(truckOneEl.style.left || 0) - 20 + "px";
+        truckTwoEl.style.left = parseFloat(truckTwoEl.style.left || 0) - 40 + "px";
+        truckThreeEl.style.left = parseFloat(truckThreeEl.style.left || 0) - 50 + "px";
+        logOneEl.style.left = parseFloat(logOneEl.style.left || 0) - 40 + "px";
+        logThreeEl.style.left = parseFloat(logThreeEl.style.left || 0) - 15 + "px";
+
+        await sleep(200);
+
+        if (parseFloat(truckOneEl.style.left || 0) < -900) {
+            truckOneEl.style.left = 500;
+        }
+        if (parseFloat(truckTwoEl.style.left || 0) < -900) {
+            truckTwoEl.style.left = 500;
+        }
+        if (parseFloat(truckThreeEl.style.left || 0) < -900) {
+            truckThreeEl.style.left = 500;
+        }
+        if (parseFloat(logOneEl.style.left || 0) < 0) {
+            logOneEl.style.left = 1500;
+        }
+        if (parseFloat(logThreeEl.style.left || 0) < 0) {
+            logThreeEl.style.left = 1500;
+        }
+
     }
+ 
+}
+
+const carOneEl = document.getElementById("carOneID");
+const carTwoEl = document.getElementById("carTwoID");
+const logTwoEl = document.getElementById("logTwo");
+
+async function driveRight() {
+
+    while (1) {
+
+        carOneEl.style.left = parseFloat(carOneEl.style.left || 0) + 50 + "px";
+        carTwoEl.style.left = parseFloat(carTwoEl.style.left || 0) + 100 + "px";
+        logTwoEl.style.left = parseFloat(logTwoEl.style.left || 0) + 30 + "px";
+
+        await sleep(300);
+
+        if (parseFloat(carOneEl.style.left || 0) > 900) {
+            carOneEl.style.left = -500;
+        }
+
+        if (parseFloat(carTwoEl.style.left || 0) > 900) {
+            carTwoEl.style.left = -500;
+        }
+
+        if (parseFloat(logTwoEl.style.left || 0) > 1400) {
+            logTwoEl.style.left = 25;
+        }
+    }
+    
 }
 
 function sleep(ms) {
