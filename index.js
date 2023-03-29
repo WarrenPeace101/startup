468107f96e6b6ad6807e259af20b1cf3ff429afc
WarrenPeace101 <mangum.warren99@gmail.com>
2023-03-29 09:58:47 -0600

    added first part of service endpoints

---
+++ index.js
@@ -0,0 +1,16 @@
+const bcrypt = require('bcrypt');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use('/api', apiRouter);
