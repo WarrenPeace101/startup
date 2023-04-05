15f34f13efae31ea66806370f590e0041fa871f1
WarrenPeace101 <mangum.warren99@gmail.com>
2023-04-05 09:56:59 -0600

    began im

--- public/peerProxy.js
+++ public/peerProxy.js
@@ -1,7 +1,7 @@
 const { WebSocketServer } = require('ws');
 const uuid = require('uuid');
 
-class PeerProxy() {
+class PeerProxy {
 
     constructor(httpServer) {
         
@@ -15,7 +15,46 @@ class PeerProxy() {
 
         let connections = [];
 
-        
+        wss.on('connection', (ws) => {
+          const connection = { id: uuid.v4(), alive: true, ws: ws };
+          connections.push(connection);
+
+        ws.on('message', function message(data) {
+          connections.forEach((c) => {
+            if (c.id !== connection.id) {
+              c.ws.send(data);
+            }
+          });
+        });
+
+      ws.on('close', () => {
+        connections.findIndex((o, i) => {
+          if (o.id === connection.id) {
+            connections.splice(i, 1);
+            return true;
+          }
+        });
+      });
+
+      ws.on('pong', () => {
+        connection.alive = true;
+      });
+
+    });
+
+    setInterval(() => {
+      connections.forEach((c) => {
+        // Kill any connection that didn't respond to the ping last time
+        if (!c.alive) {
+          c.ws.terminate();
+        } else {
+          c.alive = false;
+          c.ws.ping();
+        }
+      });
+    }, 10000);
 
 }
-}
+}//class PeerProxy
+
+module.exports = { PeerProxy };
