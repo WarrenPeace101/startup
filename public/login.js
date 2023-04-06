13c59dbf117ff270434d237bc20c46e7fb207c82
WarrenPeace101 <mangum.warren99@gmail.com>
2023-04-05 21:28:03 -0600

    added a random quote to the front page :D

--- public/login.js
+++ public/login.js
@@ -64,3 +64,28 @@ if (response?.status === 200) {
     }
     return null;
   }
+
+  function callService(url, displayCallback) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        displayCallback(data);
+      });
+  }
+
+  function displayQuote(data) {
+    const containerEl = document.querySelector("#quoteText");
+  
+    const quoteEl = document.createElement("p");
+    quoteEl.classList.add("quote");
+    const authorEl = document.createElement("p");
+    authorEl.classList.add("author");
+  
+    quoteEl.textContent = data.content;
+    authorEl.textContent = data.author;
+  
+    containerEl.appendChild(quoteEl);
+    containerEl.appendChild(authorEl);
+  }
+
+  callService("https://api.quotable.io/random", displayQuote);
